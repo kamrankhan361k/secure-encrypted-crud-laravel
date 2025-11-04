42d191c4a8d9837ca7668498574f5a8bc8d19876
unknown <kamrankhan3.61k@gmail.com>
2025-11-04 21:03:42 +0500

    adding the route code in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Http\Controllers\SecureDataController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Secure Data Routes
+Route::resource('secure-data', SecureDataController::class);
+Route::get('secure-data/verify/{id}', [SecureDataController::class, 'verifyEncryption'])
+     ->name('secure-data.verify');
+Route::post('secure-data/{id}/toggle-status', [SecureDataController::class, 'toggleStatus'])
+     ->name('secure-data.toggle-status');
+
+// Dashboard Route
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
